e7e04d4ed8cde3ea625a36698e7a9bd1c1c00ee7
jakekoroman <jakekoroman@gmail.com>
2021-11-25 16:12:21 -0500

    Added gruber-darker to emacs themes

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -25,7 +25,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox)
+;;(setq doom-theme 'doom-gruvbox)
+(setq doom-theme 'gruber-darker)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -52,3 +53,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Removes exit confirmation
+(setq confirm-kill-emacs nil)
