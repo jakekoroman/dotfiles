064ed7cca76b97d867b55a9e5d0aa03dbeedd24a
jakekoroman <jakekoroman@gmail.com>
2022-07-07 14:34:06 -0400

    Minor changes.

--- dwm/config.h
+++ dwm/config.h
@@ -1,17 +1,17 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx			= 1;        /* border pixel of windows */
-static const unsigned int snap				= 32;       /* snap pixel */
-static const unsigned int systraypinning	= 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft		= 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing	= 10;   /* systray spacing */
-static const int systraypinningfailfirst	= 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray				= 1;     /* 0 means no systray */
-static const int showbar					= 1;     /* 0 means no bar */
-static const int topbar						= 1;     /* 0 means bottom bar */
-static const char *fonts[]					= { "monospace:size=10" };
-static const char dmenufont[]				= "monospace:size=10";
+static const unsigned int borderpx			= 1;						/* border pixel of windows */
+static const unsigned int snap				= 32;						/* snap pixel */
+static const unsigned int systraypinning	= 1;						/* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft		= 0;						/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing	= 10;						/* systray spacing */
+static const int systraypinningfailfirst	= 1;						/* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray				= 1;						/* 0 means no systray */
+static const int showbar					= 1;						/* 0 means no bar */
+static const int topbar						= 1;						/* 0 means bottom bar */
+static const char *fonts[]					= { "monospace:size=10" };	/* Noto Sans Mono */
+static const char dmenufont[]				= "monospace:size=10";		/* Noto Sans Mono */
 static const char col_gray1[]				= "#222222";
 static const char col_gray2[]				= "#444444";
 static const char col_gray3[]				= "#bbbbbb";
@@ -33,7 +33,6 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "feh",      NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "discord",  NULL,       NULL,       1 << 4,       0,            1 },
 };
 
